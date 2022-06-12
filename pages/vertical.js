4200f1d1ad93139e41c7135ca1fa2ac16f1d4884
Oh TaeJun <rewrite0w0@gmail.com>
2022-06-12 23:00:43 +0900

    .......

--- pages/vertical.js
+++ pages/vertical.js
@@ -1,12 +1,28 @@
-import { config, useSpring, animated, useTrail } from "react-spring";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import styles from "../styles/vertical.module.css";
-import { useEffect, useRef, useState } from "react";
+import { config, useSpring, animated, useTrail } from 'react-spring';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import styles from '../styles/vertical.module.css';
+import { useEffect, useRef, useState } from 'react';
+
+import { MyComponent } from './motion';
+// import { CircleIndicator } from '../components/CircleIndicator';
+
+import { motion, useViewportScroll } from 'framer-motion';
+
+export const CircleIndicator = () => {
+  const { scrollYProgress } = useViewportScroll();
+
+  return (
+    <motion.path
+      d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+      style={{ pathLength: scrollYProgress }}
+    />
+  );
+};
 
 export default function Vertical() {
   const [offset, setOffset] = useState(0);
   const parallax = useRef(undefined);
-
+  // console.log(MyComponent);
   // useEffect(() => {
   //   document.addEventListener("keydown", (e) => {
   //     function ve() {
@@ -21,7 +37,6 @@ export default function Vertical() {
   // });
 
   const image = useSpring({
-    loop: true,
     from: { opacity: 0 },
     to: {
       opacity: 1,
@@ -35,6 +50,8 @@ export default function Vertical() {
 
   return (
     <div className={styles.scrollBody}>
+      <CircleIndicator />
+
       <Parallax pages={5} ref={parallax} config={config.wobby}>
         <ParallaxLayer
           offset={0}
@@ -46,10 +63,10 @@ export default function Vertical() {
             console.log(parallax.current);
           }}
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: "#00eeff",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: '#00eeff',
           }}
         >
           <header className={styles.header}>
@@ -63,24 +80,19 @@ export default function Vertical() {
             </div>
           </header>
           <main>
-            <animated.div style={image}>
+            {/* <animated.div style={image}>
               <img
                 src="https://images.unsplash.com/photo-1654714588210-b9a19d1f3d24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
                 alt="mock"
               ></img>
-            </animated.div>
-
-            {/* <h1>Layer1</h1> */}
-            {/* <img
+            </animated.div> */}
+            <MyComponent />
+            {/* <animated.img
+              style={image}
               src="https://images.unsplash.com/photo-1654714588210-b9a19d1f3d24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
               alt="mock"
-            ></img> */}
-            {/* <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
-              <animated.img
-                src="https://images.unsplash.com/photo-1654714588210-b9a19d1f3d24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="mock"
-              />
-            </Spring> */}
+            ></animated.img> */}
+            {/* <h1>Layer1</h1> */}
           </main>
           {/* <footer className={styles.footer}>
             Footer
@@ -93,17 +105,17 @@ export default function Vertical() {
           speed={0}
           onClick={() => parallax.current.scrollTo(2)}
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: "#eeaa99",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: '#eeaa99',
           }}
         >
-          {/* <h1>Layer2</h1> */}
-          <img
+          <h1>Layer2</h1>
+          {/* <img
             src="https://images.unsplash.com/photo-1474511320723-9a56873867b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1772&q=80"
             alt="mock"
-          />
+          /> */}
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -111,17 +123,17 @@ export default function Vertical() {
           speed={0}
           onClick={() => parallax.current.scrollTo(3)}
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: "#ff9900",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: '#ff9900',
           }}
         >
-          {/* <h1>Layer3</h1> */}
-          <img
+          <h1>Layer3</h1>
+          {/* <img
             src="https://images.unsplash.com/photo-1437622368342-7a3d73a34c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80"
             alt="mock"
-          />
+          /> */}
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -129,17 +141,17 @@ export default function Vertical() {
           speed={0}
           onClick={() => parallax.current.scrollTo(4)}
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: "#00eeaa",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: '#00eeaa',
           }}
         >
-          {/* <h1>Layer4</h1> */}
-          <img
+          <h1>Layer4</h1>
+          {/* <img
             src="https://images.unsplash.com/photo-1470093851219-69951fcbb533?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
             alt="mock"
-          />
+          /> */}
         </ParallaxLayer>
 
         <ParallaxLayer
@@ -147,10 +159,10 @@ export default function Vertical() {
           speed={0}
           onClick={() => parallax.current.scrollTo(0)}
           style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: "#0099ff",
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            background: '#0099ff',
           }}
         >
           <h1>Layer5</h1>
