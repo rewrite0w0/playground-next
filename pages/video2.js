33ed0712b23c5b7148bb1032133ddde5eafebd0f
oh taejun <rewrite0w0@gmail.com>
2022-07-08 17:58:43 +0900

    :)

---
+++ pages/video2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+export default function video2() {
+  return (
+    <div>
+      {/* <ReactPlayer url="https://youtu.be/iSlAIizlcb8" loop /> */}
+
+      <ReactPlayer url="https://www.youtube.com/watch?v=iSlAIizlcb8" loop />
+    </div>
+  );
+}
